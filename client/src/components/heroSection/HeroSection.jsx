24d1f2b4de2798f4b442aac1f84a2ab5bd476ac0
Miguel Arenas <migue.arenas91@gmail.com>
2024-09-13 15:47:09 -0500

    optimizing performance

--- client/src/components/heroSection/HeroSection.jsx
+++ client/src/components/heroSection/HeroSection.jsx
@@ -5,26 +5,26 @@ import { Link, useLocation } from "react-router-dom"
 
 export const HeroSection = ({ handleOpenHeroBtn, backgroundHero }) => {
 
-  const location = useLocation()
+  // const location = useLocation()
 
-  const HeroLayoutStyle =
-    location.pathname === "/"
-      ? "relative z-10 flex flex-col justify-center items-center h-full text-center mt-4"
-      : location.pathname === "/como-funciona"
-      ? "relative z-10 flex flex-col justify-center items-center h-full text-center mt-10 gap-1.5 lg:gap-5"
-        : "relative z-10 flex flex-col justify-center items-center h-full text-center mt-4";
+  // const HeroLayoutStyle =
+  //   location.pathname === "/"
+  //     ? "relative z-10 flex flex-col justify-center items-center h-full text-center mt-4"
+  //     : location.pathname === "/como-funciona"
+  //     ? "relative z-10 flex flex-col justify-center items-center h-full text-center mt-10 gap-1.5 lg:gap-5"
+  //       : "relative z-10 flex flex-col justify-center items-center h-full text-center mt-4";
   
-  const heroBtnLayout =
-    location.pathname === "/"
-      ? " text-white hover:bg-customBlue py-3 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg cta-btn"
-      : location.pathname === "/como-funciona"
-      ? " text-white hover:bg-customBlue py-2.5 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg cta-btn mt-16"
-      : " text-white hover:bg-customBlue py-3 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg cta-btn";
+  // const heroBtnLayout =
+  //   location.pathname === "/"
+  //     ? " text-white hover:bg-customBlue py-3 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg cta-btn"
+  //     : location.pathname === "/como-funciona"
+  //     ? " text-white hover:bg-customBlue py-2.5 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg cta-btn mt-16"
+  //     : " text-white hover:bg-customBlue py-3 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg cta-btn";
 
-  const linkRedirection =
-    location.pathname === "/como-funciona"
-      ? "#howItWorks-science"
-      : "#productRedirect";
+  // const linkRedirection =
+  //   location.pathname === "/como-funciona"
+  //     ? "#howItWorks-science"
+  //     : "#productRedirect";
 
   return (
     <div className="relative bg-[#4C4C4C] h-screen text-white overflow-hidden">
@@ -38,7 +38,7 @@ export const HeroSection = ({ handleOpenHeroBtn, backgroundHero }) => {
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
 
-      <div className={HeroLayoutStyle}>
+      <div className="relative z-10 flex flex-col justify-center items-center h-full text-center mt-4">
         <span className=" hero-title">Ejercitador de Mandibula</span>
         <h1 className="hero-subtitle">
           <Link to={"/"} className="linkName">
@@ -49,8 +49,8 @@ export const HeroSection = ({ handleOpenHeroBtn, backgroundHero }) => {
           Esculpe y define tu mandíbula para transformar tu atractivo.
         </p>
         <a
-          href={linkRedirection}
-          className={heroBtnLayout}
+          href="/#productRedirect"
+          className="cta-btn"
           onClick={handleOpenHeroBtn}
         >
           Conocelo
