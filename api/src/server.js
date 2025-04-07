f019eb1306702dd4712d9d889737dea2f0f0d256
Miguel Arenas <migue.arenas91@gmail.com>
2025-04-07 15:21:24 -0500

    fixing env files

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,5 @@
-import dotenv from "dotenv"
 import { app } from "./app/app.js";
 import { PORT } from "./config/server.config.js";
-dotenv.config()
 
 const connectedServer = app.listen(PORT, () => console.log(`Listening on ${PORT} Port`))
 export default connectedServer
